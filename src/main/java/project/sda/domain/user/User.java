935757cce414c6d9b9387add960614fa8fb315a5
Wojtek <heliioss2020@gmail.com>
2022-02-13 00:02:38 +0100

    BAZALOTOW-7: Create User model (fix bug)

--- src/main/java/project/sda/domain/user/User.java
+++ src/main/java/project/sda/domain/user/User.java
@@ -1,31 +1,96 @@
 package project.sda.domain.user;
 
+import com.sun.istack.NotNull;
+
 import javax.persistence.*;
 
 @Entity
-@Table (name = "USER")
+@Table(name = "USERS")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private Integer id;
 
-    @Column(name= "user_name")
+    @NotNull
+    @Column(name = "user_name")
     private String userName;
 
+    @NotNull
     @Column(name = "password")
     private String password;
 
-    @Column(name="first_name")
+    @NotNull
+    @Column(name = "firstname")
     private String firstName;
 
-    @Column(name = "last_name")
+    @NotNull
+    @Column(name = "lastname")
     private String lastName;
 
+    @NotNull
     @Column(name = "role")
     private int role;
 
     @Column(name = "blocked")
-    private int blocked;
+    private boolean blocked;
 
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    public boolean isBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        this.blocked = blocked;
+    }
 }
