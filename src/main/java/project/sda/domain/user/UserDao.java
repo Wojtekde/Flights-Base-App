484d3e0cf7242882950ef78db4aeaeb17ffeddf7
Wojtek <heliioss2020@gmail.com>
2022-02-13 12:54:06 +0100

    BAZALOTOW-23: CRUD for User

---
+++ src/main/java/project/sda/domain/user/UserDao.java
@@ -0,0 +1,18 @@
+package project.sda.domain.user;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserDao {
+    void save(User entity);
+
+    void update(User entity);
+
+    void deleteById(int id);
+
+    User findOne(int id);
+
+    Optional<User> findOneOptional(int id);
+
+    List<User> findAll();
+}
---
+++ src/main/java/project/sda/domain/user/UserDaoImpl.java
@@ -0,0 +1,67 @@
+package project.sda.domain.user;
+
+import project.sda.infrastructure.HibernateUtil;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import java.util.List;
+import java.util.Optional;
+
+public class UserDaoImpl implements UserDao {
+
+    private final EntityManager entityManager;
+
+    public UserDaoImpl() {
+        this.entityManager = HibernateUtil.getSessionFactory().createEntityManager();
+    }
+
+    @Override
+    public void save(User entity) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            entityManager.persist(entity);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            transaction.rollback();
+            throw e;
+        }
+    }
+
+    @Override
+    public void update(User entity) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            entityManager.merge(entity);
+            transaction.commit();
+        } catch (RuntimeException e) {
+            transaction.rollback();
+            throw e;
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Optional <User>  findOneOptional = findOneOptional(id);
+        if (findOneOptional.isPresent()){
+            entityManager.remove(findOneOptional.get());
+        }
+    }
+
+    @Override
+    public User findOne(int id) {
+        return entityManager.find(User.class, id);
+    }
+
+    @Override
+    public Optional<User> findOneOptional(int id) {
+        return Optional.ofNullable(entityManager.find(User.class, id));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return entityManager.createQuery("FROM User", User.class).getResultList();
+    }
+}
+
